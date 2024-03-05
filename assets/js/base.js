21cfee390e9e56c05426f59c909772fc2a9841a0
PythJScharm <145132379+LandonPerkinsProsser@users.noreply.github.com>
2024-03-05 09:15:02 -0500

    Add files via upload

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,1 +1,54 @@
-
+let siteName = "Prosser Unblocked Games";
+//Ready
+window.onload = function () {
+}
+function setTitle(title) {
+  if (title == undefined)
+    document.title = siteName;
+  else
+    document.title = title + " - " + siteName;
+}
+function setInnerHTML(elm, html) {
+  elm.innerHTML = html;
+  Array.from(elm.querySelectorAll("script")).forEach(oldScript => {
+    const newScript = document.createElement("script");
+    Array.from(oldScript.attributes)
+      .forEach(attr => newScript.setAttribute(attr.name, attr.value));
+    newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+    oldScript.parentNode.replaceChild(newScript, oldScript);
+  });
+}
+function loadGameList(containerId, count) {
+  if (!count)
+    count = 1000;
+  var mainContent = document.getElementById(containerId);
+  var fetchTemplate = fetch("templates/project-entry.htm").then(function (templateFile) { return templateFile.text(); });
+  var fetchProjectJson = fetch("assets/data/games-list.json?v1.2").then(function (response) { return response.json(); });
+  Promise.all([fetchTemplate, fetchProjectJson]).then(function (results) {
+    var template = results[0];
+    var projects = results[1];
+    for (var i = 0; i < projects.length && i < count; i++) {
+      var item = projects[i];
+      var entry = template;
+      var tagsHtml = "";
+      var linksHtml = "";
+      if (item.tags) {
+        for (var t = 0; t < item.tags.length; t++) {
+          tagsHtml += '<a href="#" class="pill">' + item.tags[t] + '</a>';
+        }
+      }
+      if (item.links) {
+        for (var l = 0; l < item.links.length; l++) {
+          linksHtml += '<a target="_blank" href="' + item.links[l].url + '"><img class="svg-icon" src="' + item.links[l].icon + '"></a>';
+        }
+      }
+      entry = entry.replace("{{name}}", item.name);
+      entry = entry.replace("{{url}}", item.url);
+      entry = entry.replace("{{icon}}", item.icon);
+      entry = entry.replace("{{description}}", item.description);
+      entry = entry.replace("{{tags}}", tagsHtml);
+      entry = entry.replace("{{links}}", linksHtml);
+      mainContent.innerHTML += entry;
+    }
+  });
+}
---
+++ assets/js/router.js
@@ -0,0 +1,57 @@
+let routes = {};
+let templates = {};
+function view(url, title) {
+    let appDiv = document.getElementById("main-content");
+    fetch(url).then(function (page) {
+        return page.text();
+    }).then(function (html) {
+        setInnerHTML(appDiv, html);
+        setTitle(title);
+    });
+}
+function route(path, template) {
+    if (typeof template === 'function') {
+        return routes[path] = template;
+    }
+    else if (typeof template === 'string') {
+        return routes[path] = templates[template];
+    } else {
+        return;
+    };
+};
+function template(name, templateFunction) {
+    return templates[name] = templateFunction;
+};
+function resolveRoute(route) {
+    try {
+        return routes[route];
+    } catch (e) {
+        throw new Error(`Route ${route} not found`);
+    };
+};
+function router(evt) {
+    let url = window.location.hash.slice(1) || '/';
+    let routeFunc = resolveRoute(url);
+    console.log(url);
+    routeFunc();
+};
+function initRoutes() {
+    // Define templates here
+    template('home', () => view("pages/home.htm"));
+    template('games', () => view("pages/games.htm", "Games"));
+    template('about', () => view("pages/about.htm", "About Me"));
+    template('contact', () => view("pages/contact.htm", "Contact"));
+    template('games-eaglercraft', () => view("pages/games/eaglercraft.htm", "Eaglercraft"));
+    template('games-super-mario-sixty-four', () => view("pages/games/sm64.htm", "Super Mario 64"));
+    // Define routes here
+    route('/', 'home');
+    route('/games', 'games');
+    route('/about', 'about');
+    route('/contact', 'contact');
+    route('/games/eaglercraft', 'games-eaglercraft');
+    route('/games/super-mario-sixty-four', 'games-super-mario-sixty-four');
+}
+initRoutes();
+// Add event listeners
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
